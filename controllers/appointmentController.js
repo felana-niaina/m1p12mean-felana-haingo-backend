413808a9d1690ebffbd2fb4c4a2ab40ff3912cd4
felana-niaina <nirina.felananiaina@gmail.com>
2025-04-02 22:05:53 +0300

    test

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -317,7 +317,7 @@ export const assignMechanic = async (req, res) => {
 // Récupération la liste des rendez-vous par client
 export const getAppointmentsByClient = async (req, res) => {
   const { clientId } = req.query; 
-
+  console.log (clientId)
   try {
     if (!clientId) {
       return res.status(400).json({ message: "L'ID du client est requis." });
